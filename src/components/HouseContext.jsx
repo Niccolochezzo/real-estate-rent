b47c645186cc1a9353a6d30062138102a79c49e5
niccolo chezzo <niccolochezzo@gmail.com>
2023-07-10 23:27:26 +0400

    added dropdowns and context to it, added empty hou

--- src/components/HouseContext.jsx
+++ src/components/HouseContext.jsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState, createContext } from "react";
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+import { housesData } from "../data";
+
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState("Location (any)");
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState("Property type (any)");
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState("Price range (any)");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const allCountries = houses.map((house) => {
+      return house.country;
+    });
+
+    const uniqueCountries = ["Location (any)", ...new Set(allCountries)];
+    setCountries(uniqueCountries);
+  }, []);
+
+  useEffect(() => {
+    const allProperties = houses.map((house) => {
+      return house.type;
+    });
+
+    const uniqueProperties = ["Location (any)", ...new Set(allProperties)];
+    setProperties(uniqueProperties);
+  }, []);
+
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <HouseContext.Provider
+      value={{
+        country,
+        setCountries,
+        countries,
+        property,
+        setProperty,
+        properties,
+        price,
+        setPrice,
+        houses,
+        loading,
+        handleClick,
+      }}
+    >
+      {children}
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
